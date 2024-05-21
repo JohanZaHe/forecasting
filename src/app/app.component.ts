95e92bbb9660971dd00633d9961f1ef2f2b5e337
System Administrator <root@MacBookProM1Pro-JohanZapata.local>
2024-05-21 17:22:12 -0500

    Pushing the initial project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'forecasting';
+  locations = [
+    {
+      name: 'District of Columbia Forecast ( LWX )',
+      flagPath: '',
+      route: 'district_of_columbia.png',
+    },
+    {
+      name: 'Kansas Forecast ( TOP )',
+      flagPath: '',
+      route: 'kansas.png',
+    },
+  ];
 }
---
+++ src/app/wheater/wheater.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wheater',
+  standalone: true,
+  imports: [],
+  templateUrl: './wheater.component.html',
+  styleUrl: './wheater.component.scss'
+})
+export class WheaterComponent {
+
+}
